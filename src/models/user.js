62bf1f7beb8aa5750f707fcc84ff28cde1b39316
Jigme Dukpa <jigmedukpa@Jigmes-MacBook-Pro.local>
2020-03-21 13:16:41 -0400

    upgraded joi

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,6 @@
 const config = require('config');
 const jwt = require('jsonwebtoken');
-const Joi = require('joi');
+const Joi = require('@hapi/joi');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
@@ -50,13 +50,13 @@ UserSchema.methods.setPassword = async function(password) {
 const User = mongoose.models.User || mongoose.model('User', UserSchema);
 
 function validateUser(user) {
-  const schema = {
+  const schema = Joi.object({
     name: Joi.string().min(3).max(50).required(),
     email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().min(3).max(255).required()
-  };
+  });
 
-  return Joi.validate(user, schema);
+  return schema.validate(user);
 }
 
 exports.User = User; 
