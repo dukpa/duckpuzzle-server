12a5c08d9a22691e1c486ac6c745275da5f2829e
Jigme Dukpa <jigmedukpa@Jigmes-MacBook-Pro.local>
2020-03-31 22:24:51 -0400

    removed only

--- test/api/request.test.js
+++ test/api/request.test.js
@@ -1,7 +1,7 @@
 const {expect, request} = require('chai');
 const app = require('app');
 
-describe.only('hello', function() {
+describe('hello', function() {
   it('should receive put', async function() {
     let res = await request(app).put('/request');
     expect(res).status(200);
