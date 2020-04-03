bf70d96de43759f763e86f661cadad226852d419
Jigme Dukpa <jigmedukpa@Jigmes-MacBook-Pro.local>
2020-04-02 23:02:00 -0400

    moved login tests under post

--- test/api/login.test.js
+++ test/api/login.test.js
@@ -2,29 +2,31 @@ const { expect, request } = require('chai');
 const app = require('app');
 
 describe('/login', function() {
-  it('should return a token', async function() {
-    let user = 'test@example.com';
-    let password = 'test';
-    let res = await request(app).post('/login').send({
-      email: user,
-      password
+  describe('POST', function() {
+    it('should return a token', async function() {
+      let user = 'test@example.com';
+      let password = 'test';
+      let res = await request(app).post('/login').send({
+        email: user,
+        password
+      });
+      expect(res).status(200);
+      expect(res.body.data.token).to.be.string;
     });
-    expect(res).status(200);
-    expect(res.body.data.token).to.be.string;
-  });
-
-  it('should raise 401 if failed login', async function() {
-    let user = 'fake_user@example.com';
-    let password = 'test';
-    let res = await request(app).post('/login').send({
-      email: user,
-      password
+  
+    it('should raise 401 if failed login', async function() {
+      let user = 'fake_user@example.com';
+      let password = 'test';
+      let res = await request(app).post('/login').send({
+        email: user,
+        password
+      });
+      expect(res).status(401);
+      expect(res.body.error).to.contain({
+        code: 'BAD_USER_PASS'
+      });
     });
-    expect(res).status(401);
-    expect(res.body.error).to.contain({
-      code: 'BAD_USER_PASS'
-    });
-  });
+  })
 });
 
 //utility function for other api tests
