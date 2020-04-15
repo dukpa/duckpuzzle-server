8f8f684cda6a676e0f1978a95a101632740188e8
Jigme Dukpa <jigmedukpa@Jigmes-MacBook-Pro.local>
2020-04-14 22:06:12 -0400

    added 404 for unfound user

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,9 +4,13 @@ const express = require("express");
 const router = express.Router();
 const registration = require("../services/registration");
 const {buildResponse} = require('services/json');
+const createHttpError = require('http-errors');
 
 router.get("/current", auth, async (req, res) => {
   const user = await User.findById(req.user._id).select("_id name email");
+  if (!user) {
+    return next(createHttpError(404, 'User not found'));
+  }
   res.send(buildResponse(req, user));
 });
 
